863f546095a116bffbb425eaf837b4fb77d7c59d
Sona <s.harutyunyan1992@gmail.com>
2023-08-01 00:41:20 +0400

    Csv file

--- src/test/java/ru/netology/services/VacationCalcServiceTest.java
+++ src/test/java/ru/netology/services/VacationCalcServiceTest.java
@@ -2,32 +2,28 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class VacationCalcServiceTest {
-    @Test
-    public void shouldCalculateVacation() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "10000,3000,20000,3",
+//            "100000,60000,150000,2"
+//    })
+
+    @CsvFileSource(files = "src/test/resources/tt.csv")
+
+    public void shouldCalculateVacation(int income, int expenses, int threshold, int expected) {
         VacationCalcService service = new VacationCalcService();
 
-        int income = 10_000;
-        int expenses = 3000;
-        int threshold = 20_000;
-        int expected = 3;
+
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-   public void calculateVacation() {
-        VacationCalcService service = new VacationCalcService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
 
 }
 
